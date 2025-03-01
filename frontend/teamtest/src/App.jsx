497617eb12c6afc469da5ea05a7acc71435f4d89
Mrcoder1915 <sammynavarro001@gmail.com>
2025-03-01 23:17:15 +0800

    add login routes

--- frontend/teamtest/src/App.jsx
+++ frontend/teamtest/src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Route , Routes} from 'react-router-dom'
 import Home from './pages/home'
 import About from './pages/about'
 import Contact from './pages/contact'
+import Login from './pages/login'
 import RootLayout from './shardeComponenents/Layout/Root-Layout/RootLayout'
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
             <Route path='/' element = {<Home />}/>
             <Route path='/about' element = {<About />}/>
             <Route path='/contact' element = {<Contact />}/>
+            <Route path='/login' element = {<Login />}/>
             </Route>
         </Routes>
     </BrowserRouter>
